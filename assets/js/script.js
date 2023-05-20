35d82a5c34993f480b90bdc3bd9c67d06e425fe8
CardsClaw <joshr1979@gmail.com>
2023-05-19 20:13:03 -0500

    successful fetch for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var APIKey = "667c2def329f02a09cd4ba7bed05e4c6"
 var citySearch = document.getElementById("city");
 var searchBtn = document.getElementById("search-btn")
+
 //API 1 call to convert city name into coordinates
 function getCoordinates(cityName){
     var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid="+APIKey;
@@ -11,11 +12,24 @@ function getCoordinates(cityName){
       })
       .then(function (data) {
         console.log(data);
+        //define latitude and longitude. Data above returns array of single object. to point data to lat and lon need [0] to specify row.
         var cityLat = data[0].lat;
         var cityLon = data[0].lon;
         console.log(cityLat, cityLon);
+        getForecast(cityLat, cityLon);
       });
-      
+}
+
+function getForecast(lat, lon) {
+  var fiveDayForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+  console.log(fiveDayForecastUrl);
+  fetch(fiveDayForecastUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
 }
 
 //above currently gets 'object' with many keys including 'lat' and 'lon'
@@ -25,6 +39,5 @@ function getCoordinates(cityName){
 searchBtn.addEventListener("click", function(){
     var city=citySearch.value;
     getCoordinates(city);
-
 });
 
